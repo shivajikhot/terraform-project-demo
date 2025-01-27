780820e3d691cf89a7537b5e479ac633fa0b48e4
shivajikhot <shivajkv@gmail.com>
2025-01-27 14:42:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,28 +3,28 @@ provider "aws" {
 }
 
 # EC2 Instance
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  tags = {
-    Name = "${var.environment}-web-server"
-  }
-}
+#resource "aws_instance" "web" {
+#  ami           = var.ami_id
+#  instance_type = var.instance_type
+#  tags = {
+#    Name = "${var.environment}-web-server"
+#  }
+#}
 
 # RDS Instance
-resource "aws_db_instance" "db" {
-  allocated_storage    = var.rds_allocated_storage
-  engine               = var.rds_engine
-  engine_version       = var.rds_engine_version
-  instance_class       = var.rds_instance_class
-  db_name                 = var.rds_db_name
-  username             = var.rds_username
-  password             = var.rds_password
-  skip_final_snapshot  = true
-  tags = {
-    Name = "${var.environment}-database"
-  }
-}
+#resource "aws_db_instance" "db" {
+#  allocated_storage    = var.rds_allocated_storage
+#  engine               = var.rds_engine
+ # engine_version       = var.rds_engine_version
+#  instance_class       = var.rds_instance_class
+#  db_name                 = var.rds_db_name
+#  username             = var.rds_username
+#  password             = var.rds_password
+#  skip_final_snapshot  = true
+#  tags = {
+#    Name = "${var.environment}-database"
+#  }
+#}
 
 # S3 Bucket
 resource "aws_s3_bucket" "static_content" {
